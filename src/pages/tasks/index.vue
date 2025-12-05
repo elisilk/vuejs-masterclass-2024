d56dd4aed286b1922a1748e2f5ee72504b84ac26
Eli Silk <37377295+elisilk@users.noreply.github.com>
2025-12-05 11:18:17 -0500

    Setup tasks and project tables; improved UI with shadcn

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -6,19 +6,17 @@ import type { ColumnDef } from '@tanstack/vue-table'
 import DataTable from '@/components/ui/data-table/DataTable.vue'
 import { RouterLink } from 'vue-router'
 
-const projects = ref<Tables<'projects'>[] | null>(null)
+const tasks = ref<Tables<'tasks'>[] | null>(null)
 
 ;(async () => {
-  const { data, error } = await supabase.from('projects').select()
+  const { data, error } = await supabase.from('tasks').select()
 
   if (error) console.log(error)
 
-  projects.value = data
-
-  console.log('Projects :', projects.value)
+  tasks.value = data
 })()
 
-const columns: ColumnDef<Tables<'projects'>>[] = [
+const columns: ColumnDef<Tables<'tasks'>>[] = [
   {
     accessorKey: 'name',
     header: () => h('div', { class: 'text-left' }, 'Name'),
@@ -26,7 +24,7 @@ const columns: ColumnDef<Tables<'projects'>>[] = [
       return h(
         RouterLink,
         {
-          to: `/projects/${row.original.slug}`,
+          to: `/tasks/${row.original.id}`,
           class: 'text-left font-medium hover:bg-muted block w-full',
         },
         () => row.getValue('name'),
@@ -38,6 +36,20 @@ const columns: ColumnDef<Tables<'projects'>>[] = [
     header: () => h('div', { class: 'text-left' }, 'Status'),
     cell: ({ row }) => {
       return h('div', { class: 'text-left font-medium' }, row.getValue('status'))
+    },
+  },
+  {
+    accessorKey: 'due_date',
+    header: () => h('div', { class: 'text-left' }, 'Due Date'),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, row.getValue('due_date'))
+    },
+  },
+  {
+    accessorKey: 'project_id',
+    header: () => h('div', { class: 'text-left' }, 'Project'),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, row.getValue('project_id'))
     },
   },
   {
@@ -55,5 +67,5 @@ const columns: ColumnDef<Tables<'projects'>>[] = [
 </script>
 
 <template>
-  <DataTable v-if="projects" :columns="columns" :data="projects" />
+  <DataTable v-if="tasks" :columns="columns" :data="tasks" />
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,24 @@ import { fileURLToPath, URL } from 'node:url'
 import VueRouter from 'unplugin-vue-router/vite'
 
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [VueRouter(), vue(), vueDevTools()],
+  plugins: [
+    VueRouter(),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (element) => element.startsWith('iconify-icon'),
+        },
+      },
+    }),
+    tailwindcss(),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
